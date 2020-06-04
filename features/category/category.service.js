d624c0fe38ffc426b3fc45eec37409abb830f2ed
Sanaullahbugti <sanaullahbugti821@gmail.com>
2020-06-04 23:59:54 +0500

    add item

--- features/category/category.service.js
+++ features/category/category.service.js
@@ -11,3 +11,8 @@ exports.getAllCategories = async ( query ) => {
     const [err, categories] = await to( Category.find( query ) )
     return { err, categories }
 }
+
+exports.updateCategories = async ( query, fields ) => {
+    const [err, updatedCategory] = await to( Category.update( query, fields ) )
+    return { err, updatedCategory }
+}
